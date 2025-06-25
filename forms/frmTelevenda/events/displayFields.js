4ff49a6bff7e8c78617325a0afbcfbd0ea3f283b
felipeluis223 <lf9072962@gmail.com>
2025-06-25 11:33:19 -0300

    feat: input de responsaveis e datas.

---
+++ forms/frmTelevenda/events/displayFields.js
@@ -0,0 +1,17 @@
+function displayFields(form,customHTML){
+    
+    
+    var allFields = [
+        "dataVenda",
+        "valorVenda",
+        "formaPagamentoVenda",
+        "lojaResponsavel",
+        "obsVenda",
+        "radioTypes",
+        "obsFinanceiro",
+        "checkboxVisto"
+    ];
+
+
+
+}
