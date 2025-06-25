299adb8f37ba1febe454871cc9710e75100a7da3
felipeluis223 <lf9072962@gmail.com>
2025-06-25 11:09:41 -0300

    feat: Dataset sobre lojas.

---
+++ datasets/dsLojas.js
@@ -0,0 +1,19 @@
+function createDataset(fields, constraints, sortFields) {
+    var data = [
+        [ 3001, "Loja 1", "Indaiatuba" ],
+        [ 3002, "Loja 2", "Campinas" ],
+        [ 3003, "Loja 3", "São Paulo" ]
+    ];
+    
+    var ds = DatasetBuilder.newDataset();
+    ds.addColumn("ID");
+    ds.addColumn("Nome");
+    ds.addColumn("Cidade");
+
+    ds.addRow(data[0]);
+    ds.addRow(data[1]);
+    ds.addRow(data[2]);
+
+    return ds;
+
+}
