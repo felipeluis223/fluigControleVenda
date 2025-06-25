c6baa117b415fa70f765781a079aa67cd3adff72
felipeluis223 <lf9072962@gmail.com>
2025-06-25 16:14:42 -0300

    feat: habilitando os campos do formulário conforme a necessidade do grupo que o usuário participa.

--- forms/frmTelevenda/events/displayFields.js
+++ forms/frmTelevenda/events/displayFields.js
@@ -1,17 +1,42 @@
 function displayFields(form,customHTML){
+    // Configuração dos Grupos e Datasets:
+    var user = getValue("WKUser");
+    var constraints = [ DatasetFactory.createConstraint("colleagueGroupPK.colleagueId", user, user, ConstraintType.MUST) ];
+    var groupDataset = DatasetFactory.getDataset("colleagueGroup", null, constraints, null);
+    var groups = [ "grpLoja1", "grpLoja2", "grpLoja3", "grpTI", "grpFinanceiro", "grpVendas"];
     
-    
-    var allFields = [
-        "dataVenda",
-        "valorVenda",
-        "formaPagamentoVenda",
-        "lojaResponsavel",
-        "obsVenda",
-        "radioTypes",
-        "obsFinanceiro",
-        "checkboxVisto"
-    ];
+    // Percorrendo os grupos do usuário e armazenando conforme a necessidade:
+    var userGroup = {};
+    for(var index=0; index<=groupDataset.rowsCount; index++){
+        var groupId = groupDataset.getValue(index, "colleagueGroupPK.groupId");
+        for (var i=0; i<groups.length; i++){
+            if(groupId == groups[i]){
+                userGroup[groups[i]] = groupId;
+            };
+        };
+    };
 
+    // Desabilitando todos inputs:
+    var allFields = ["nomeRespVenda","dataVenda","valorVenda",
+        "formaPagamentoVenda","lojaResponsavel","obsVenda","nomeRespFinanceiro",
+        "dataFinanceiro","radioTypes","obsFinanceiro","checkboxVisto"];
+    
+    for(var index=0; index<allFields.length; index++){
+        form.setEnabled(allFIelds[index], false);
+    };
+        
+    // Se o usuário for do grupo de vendas - habilita os campos de venda:
+    if("grpVendas" in userGroup){
+        user = getValue("WKUser");
+        form.setValue("nomeRespVenda", user);
+        form.setEnabled("nomeRespVenda", false);
+
+        form.Enabled("dataVenda", true);
+        form.Enabled("valorVenda", true);
+        form.Enabled("formaPagamentoVenda", true);
+        form.Enabled("lojaResponsavel", true);
+        form.Enabled("obsVenda", true);
+    }
 
 
 }
